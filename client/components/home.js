bf472c888d3d40ea427e30c2071634c33989a424
isabellenoelnelson <isabellenoelnelson@gmail.com>
2021-04-18 16:39:49 -0400

    Changed user greeting

--- client/components/home.js
+++ client/components/home.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 
 /**
  * COMPONENT
  */
-export const Home = props => {
-  const {username} = props
+export const Home = (props) => {
+	const { username } = props;
 
-  return (
-    <div>
-      <h3>Welcome, {username}</h3>
-    </div>
-  )
-}
+	return (
+		<div>
+			<h3>Hello, {username}</h3>
+		</div>
+	);
+};
 
 /**
  * CONTAINER
  */
-const mapState = state => {
-  return {
-    username: state.auth.username
-  }
-}
+const mapState = (state) => {
+	return {
+		username: state.auth.username,
+	};
+};
 
-export default connect(mapState)(Home)
+export default connect(mapState)(Home);
